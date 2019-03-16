a8bae2cb25cc902220ebfa732c585f18948a2cce
Derin Gbadebo <daireenonline@yahoo.com>
2019-03-16 16:03:54 -0700

    Cascade

--- src/com/luv2code/hibernate/demo/CreateDemo.java
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -18,7 +18,15 @@ public class CreateDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {
+			Instructor tempInstructor1 = new Instructor("Koushik", "Kokathal", "javabrains.com");
+			InstructorDetail tempIntructorDetails1 = new InstructorDetail("http://javaBrains.com/youtube", "Fishing");
+			Instructor tempInstructor2 = new Instructor("Chad", "Darby", "luv2code.com");
+			InstructorDetail tempIntructorDetails2 = new InstructorDetail("http://luv2code.com/youtube", "Loves to code!");
+			tempInstructor1.setInstructorDetail(tempIntructorDetails1);
+			tempInstructor2.setInstructorDetail(tempIntructorDetails2);
 			session.beginTransaction();
+			session.save(tempInstructor1);
+			session.save(tempInstructor2);
 			session.getTransaction().commit();
 		} finally {
 			factory.close();
